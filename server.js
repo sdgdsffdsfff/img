b54a9f97b39bee6bb5b83069bfdf639a4fabb41c
刘兵 <lb1064@qq.com>
2014-12-15 23:40:20 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+
+/*
+*
+*	GLOBAL path helpers
+*
+*/
+
+GLOBAL.paths = require('./config/paths');
+
+
+
+/*
+*
+* Express
+*
+*/
+
+var express = require('express');
+var app = express();
+
+
+/*
+*
+* Configuration
+*
+*/
+
+require(GLOBAL.paths.getConfig('express'))(app, express);
+console.log('App Environment', app.get('env'));
+
+
+
+/*
+*
+* Routes
+*
+*/
+
+require(GLOBAL.paths.getRoute())(app, express);
+
+
+
+/*
+*
+* Server
+*
+*/
+
+console.log(Date.now(), 'Running Node.js ' + process.version + ' with flags "' + process.execArgv.join(' ') + '"');
+app.listen(app.get('port'), function() {
+	console.log(Date.now(), 'Express server listening on port ' + app.get('port') + ' in mode: ' + process.env.NODE_ENV);
+	// console.log(Date.now(), 'ENVIRONMENT:', process.env);
+});
---
+++ services/img.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = function (width, height, callback) {
+    var Canvas = require('canvas')
+        , canvas = new Canvas(width, height)
+        , ctx = canvas.getContext('2d');
+
+    ctx.fillStyle="#FF0000";
+    ctx.fillRect(0,0,width,height);
+
+
+    // 设置字体
+    ctx.font = "30px Arial";
+    // 设置对齐方式
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    // 设置填充颜色
+    ctx.fillStyle = "#008600";
+    // 设置字体内容，以及在画布上的位置
+    ctx.fillText(width+'x'+height, width/2, height/2);
+
+
+    var buf = canvas.toBuffer();
+
+    callback(null, buf);
+};
+
+
+
+
