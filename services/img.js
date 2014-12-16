3b917da552e2cad17c619671881ee0784532bef0
刘兵 <lb1064@qq.com>
2014-12-16 09:57:00 +0800

    Initial commit 修改颜色

--- services/img.js
+++ services/img.js
@@ -12,15 +12,19 @@ module.exports = function (width, height, callback) {
     if (width > 150) {
         // 设置字体
         ctx.font = "30px Arial";
-        // 设置对齐方式
-        ctx.textAlign = "center";
-        ctx.textBaseline = 'middle';
-        // 设置填充颜色
-        ctx.fillStyle = "#008600";
-        // 设置字体内容，以及在画布上的位置
-        ctx.fillText(width + 'x' + height, width / 2, height / 2);
+
+    } else {
+        ctx.font = "12px Arial";
+
     }
 
+    // 设置对齐方式
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    // 设置填充颜色
+    ctx.fillStyle = "#008600";
+    // 设置字体内容，以及在画布上的位置
+    ctx.fillText(width + 'x' + height, width / 2, height / 2);
 
     var buf = canvas.toBuffer();
 
