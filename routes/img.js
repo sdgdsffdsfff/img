ade425669af62a14a73f6ffb3ab9513746f23aa2
刘兵 <lb1064@qq.com>
2014-12-16 13:42:07 +0800

    图片宽高限制为2000

--- routes/img.js
+++ routes/img.js
@@ -4,48 +4,55 @@ var img = require(GLOBAL.paths.getService('img'));
 
 
 function isInt(n) {
-	return (typeof n === 'number' && n % 1 == 0);
+    return (typeof n === 'number' && n % 1 == 0);
 }
 
 
 function reportToGA(req, width, height) {
-	var referer = req.get('referer');
-	var ua = require('universal-analytics');
-	var uaUUID = (req.cookies && req.cookies.uaUUID) ? req.cookies.uaUUID : null;
-	var visitor = ua('UA-51384-17', uaUUID);
+    var referer = req.get('referer');
+    var ua = require('universal-analytics');
+    var uaUUID = (req.cookies && req.cookies.uaUUID) ? req.cookies.uaUUID : null;
+    var visitor = ua('UA-51384-17', uaUUID);
 
-	var dims = width + 'x' + height;
+    var dims = width + 'x' + height;
 
-	// Visitor#event(category, action, label, value)
-	visitor.event('hotlink', referer, dims).send();
+    // Visitor#event(category, action, label, value)
+    visitor.event('hotlink', referer, dims).send();
 }
 
 
-module.exports = function(req, res) {
-	var width = (req.params.width) ? parseInt(req.params.width) : undefined;
-	var height = (req.params.height) ? parseInt(req.params.height) : width;
+module.exports = function (req, res) {
+    var width = (req.params.width) ? parseInt(req.params.width) : undefined;
+    var height = (req.params.height) ? parseInt(req.params.height) : width;
 
 
-	if (!isInt(width) || !isInt(height)) {
-		res.status(404);
-		res.send('<h1>Not Found</h1>');
-	}
-	else {
-		img(width, height, function (err, svgString) {
-			var cacheTime = 60 * 60 * 24 * 7; // 7 Days
-			var expires = new Date(Date.now() + (cacheTime * 1000)).toUTCString();
+    if (!isInt(width) || !isInt(height)) {
+        res.status(404);
+        res.send('<h1>Not Found</h1>');
+    }
+    else {
 
-			res.writeHead(200, {
-				'Content-Type': 'image/png',
-				'Cache-Control': 'public, max-age=' + (cacheTime),
-				'Expires': expires
-			});
+        if (width > 2000 || height > 2000) {
+            res.status(404);
+            res.end('<h1>图片设置太大</h1>');
+            return;
+        }
 
-			res.end(svgString);
+        img(width, height, function (err, svgString) {
+            var cacheTime = 60 * 60 * 24 * 7; // 7 Days
+            var expires = new Date(Date.now() + (cacheTime * 1000)).toUTCString();
+
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Cache-Control': 'public, max-age=' + (cacheTime),
+                'Expires': expires
+            });
+
+            res.end(svgString);
 
 
-			process.nextTick(reportToGA.bind(null, req, width, height));
+            process.nextTick(reportToGA.bind(null, req, width, height));
 
-		});
-	}
+        });
+    }
 };
