09e7fd23a487d02c36d91202bf063361337809e2
刘兵 <lb1064@qq.com>
2014-12-16 09:14:01 +0800

    Initial commit 修改颜色

--- services/img.js
+++ services/img.js
@@ -5,19 +5,21 @@ module.exports = function (width, height, callback) {
         , canvas = new Canvas(width, height)
         , ctx = canvas.getContext('2d');
 
-    ctx.fillStyle="#ccc";
-    ctx.fillRect(0,0,width,height);
+    ctx.fillStyle = "#ccc";
+    ctx.fillRect(0, 0, width, height);
 
 
-    // 设置字体
-    ctx.font = "30px Arial";
-    // 设置对齐方式
-    ctx.textAlign = "center";
-    ctx.textBaseline = 'middle';
-    // 设置填充颜色
-    ctx.fillStyle = "#008600";
-    // 设置字体内容，以及在画布上的位置
-    ctx.fillText(width+'x'+height, width/2, height/2);
+    if (width > 150) {
+        // 设置字体
+        ctx.font = "30px Arial";
+        // 设置对齐方式
+        ctx.textAlign = "center";
+        ctx.textBaseline = 'middle';
+        // 设置填充颜色
+        ctx.fillStyle = "#008600";
+        // 设置字体内容，以及在画布上的位置
+        ctx.fillText(width + 'x' + height, width / 2, height / 2);
+    }
 
 
     var buf = canvas.toBuffer();
