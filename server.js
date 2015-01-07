e10020c438ab4e590ca99bbb02acf81bda38fa0b
lb1064 <lb1064@qq.com>
2015-01-07 09:41:49 +0800

    多线程优化

--- server.js
+++ server.js
@@ -1,53 +1,65 @@
-
 /*
-*
-*	GLOBAL path helpers
-*
-*/
+ *
+ *	GLOBAL path helpers
+ *
+ */
 
 GLOBAL.paths = require('./config/paths');
 
 
-
 /*
-*
-* Express
-*
-*/
-
+ *
+ * Express
+ *
+ */
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 var express = require('express');
 var app = express();
 
 
 /*
-*
-* Configuration
-*
-*/
+ *
+ * Configuration
+ *
+ */
 
 require(GLOBAL.paths.getConfig('express'))(app, express);
 console.log('App Environment', app.get('env'));
 
 
-
 /*
-*
-* Routes
-*
-*/
+ *
+ * Routes
+ *
+ */
 
 require(GLOBAL.paths.getRoute())(app, express);
 
 
-
 /*
-*
-* Server
-*
-*/
+ *
+ * Server
+ *
+ */
 
-console.log(Date.now(), 'Running Node.js ' + process.version + ' with flags "' + process.execArgv.join(' ') + '"');
-app.listen(app.get('port'), function() {
-	console.log(Date.now(), 'Express server listening on port ' + app.get('port') + ' in mode: ' + process.env.NODE_ENV);
-	// console.log(Date.now(), 'ENVIRONMENT:', process.env);
-});
+
+if (cluster.isMaster) {
+    console.log('[master] ' + "start master...");
+
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('listening', function (worker, address) {
+        console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ":" + address.port);
+    });
+
+} else if (cluster.isWorker) {
+    console.log('[worker] ' + "start worker ..." + cluster.worker.id);
+    app.listen(app.get('port'), function () {
+        console.log('worker' + cluster.worker.id);
+        console.log(Date.now(), 'Express server listening on port ' + app.get('port') + ' in mode: ' + process.env.NODE_ENV);
+    });
+}
+
