b85e42af627cdb0493832ee324ef16dfd2ab2c58
lb1064 <lb1064@qq.com>
2015-01-12 14:00:27 +0800

    参数处理

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,10 @@ module.exports = function(app, express) {
 
 	app.get('/:bg([0-9a-zA-z]{3,})/:width([0-9]{1,})x:height([0-9]{1,})', require(GLOBAL.paths.getRoute('img')));
 
+	app.get('/:bg([0-9a-zA-z]{3,})/:fc([0-9a-zA-z]{3,})/:width([0-9]{1,})', require(GLOBAL.paths.getRoute('img')));
+
+	app.get('/:bg([0-9a-zA-z]{3,})/:width([0-9]{1,})', require(GLOBAL.paths.getRoute('img')));
+
 
 
 
